669039e8c84d890220482e9568f17548a234899e
Yohei-Matsutan <takeda.yohei.sp@alumni.tsukuba.ac.jp>
2021-11-25 10:11:44 +0000

    [Finish]3-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,6 +4,7 @@ class BlogsController < ApplicationController
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
@@ -13,10 +14,19 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blog_path
   end
 
   def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    # 最後の(blog.id)は(blog)だけでもidを拾ってくれるみたい
+    redirect_to blog_path(blog.id)
+
   end
 
 
